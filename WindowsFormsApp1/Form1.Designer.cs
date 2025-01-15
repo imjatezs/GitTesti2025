182ea69077d444863b2f2225a9c676ab9c586842
cpplv <aliepins@inbox.lv>
2025-01-15 12:02:45 +0200

    last changes

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -53,7 +53,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(510, 248);
+            this.button2.Location = new System.Drawing.Point(358, 223);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 2;
